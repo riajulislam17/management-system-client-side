700cd5a59446964b2445d31e190620fd7b6018cc
Md. Riajul Islam <riaj17021995@gmail.com>
2022-05-10 22:33:49 +0600

    header change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="bg-dark mb-3">
             <nav className="container navbar navbar-light">
                 <div className="container-fluid">
-                    <a className="text-decoration-none" href="home"><h1 className="navbar-brand mb-0 mx-0 px-0 text-light fw-bold">HR Management</h1></a>
+                    <a className="text-decoration-none" href="home"><h1 className="navbar-brand mb-0 mx-0 px-0 text-light fw-bold"> Management System</h1></a>
                 </div>
             </nav>
         </div>
