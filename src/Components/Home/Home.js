b0530bae25c70f3e5f71bcc1fee928cf68bfa705
Md. Riajul Islam <riaj17021995@gmail.com>
2022-05-10 22:26:53 +0600

    UI changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import Insertion from '../Insertion/Insertion';
-import Retrieve from '../Retrieve/Retrieve';
+import React from "react";
+import Insertion from "../Insertion/Insertion";
+import Retrieve from "../Retrieve/Retrieve";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="container-fluid px-4">
-                <div className="row g-3">
-                    
-                    <div className="col-sm-12 col-md-7 col-lg-8">
-                        <div className="p-3 bg-light rounded border border-dark border-2"><Retrieve></Retrieve></div>
-                    </div>
-
-                    <div className="col-sm-12 col-md-5 col-lg-4">
-                        <div className="p-3 bg-light rounded border border-dark border-2"><Insertion></Insertion></div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="container-fluid px-4">
+        <div className="row g-3">
+          <div className="col-sm-12 col-md-7 col-lg-8">
+            <div className="p-3 bg-light rounded border border-dark border-2">
+              <Retrieve></Retrieve>
             </div>
+            <div>
+              <div
+                class="alert alert-danger alert-dismissible fade show mt-5"
+                role="alert"
+              >
+                <strong>Hello Dear! The data is not displayed since the server-side could not be deployed. Please follow the <a className="text-decoration-none" href="https://github.com/riajulislam17/management-system-server-side/blob/main/README.md" target="_blank" rel="noreferrer">README.md</a> file instructions.</strong> 
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="alert"
+                  aria-label="Close"
+                ></button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-12 col-md-5 col-lg-4">
+            <div className="p-3 bg-light rounded border border-dark border-2">
+              <Insertion></Insertion>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
